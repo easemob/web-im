9df29536f2a3dab7c396064a39e8f10e4db508bc
stupidkiller <ooyt@qq.com>
2015-07-24 12:50:21 +0800

    #add config.js

---
+++ easemob.im.config.js
@@ -0,0 +1,22 @@
+Easemob.im.config = {
+    /*
+        The global value set for xmpp server
+        ws://im-api.easemob.com/ws/
+        ws://im-api.sandbox.easemob.com/ws/
+        http://im-api.easemob.com/http-bind/
+        http://im-api.sandbox.easemob.com/http-bind/
+    */
+    xmppURL: null,
+    /*
+        The global value set for Easemob backend REST API
+        http://a1.easemob.com
+        http://a1.sdb.easemob.com
+    */
+    apiURL: null,
+    /*
+        连接时提供appkey
+    */
+    appkey: "easemob-demo#chatdemoui",
+    https : false,
+    wss: false
+}
