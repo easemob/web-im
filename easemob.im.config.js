b1442da74ccce6c9322db9975f3bd1e872ca1231
stupidkiller <ooyt@qq.com>
2015-08-17 11:11:38 +0800

    #release 1.0.7

--- easemob.im.config.js
+++ easemob.im.config.js
@@ -6,13 +6,13 @@ Easemob.im.config = {
         http://im-api.easemob.com/http-bind/
         http://im-api.sandbox.easemob.com/http-bind/
     */
-    xmppURL: 'http://im-api.sandbox.easemob.com/http-bind/',
+    xmppURL: 'ws://im-api.easemob.com/ws/',
     /*
         The global value set for Easemob backend REST API
         http://a1.easemob.com
         http://a1.sdb.easemob.com
     */
-    apiURL: 'http://a2.sdb.easemob.com',
+    apiURL: '',
     /*
         连接时提供appkey
     */
