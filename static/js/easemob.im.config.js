71a8cafcfa0a9f7b6d2156d1e8b6c402d99657c0
stupidkiller <ooyt@qq.com>
2016-06-17 10:25:00 +0800

    update: update xmpp server

--- static/js/easemob.im.config.js
+++ static/js/easemob.im.config.js
@@ -2,7 +2,7 @@ Easemob.im.config = {
     /*
         The global value set for xmpp server
     */
-    xmppURL: 'im-api.sdb.easemob.com',
+    xmppURL: 'im-api.sandbox.easemob.com',
     /*
         The global value set for Easemob backend REST API
         "http://a1.easemob.com"
