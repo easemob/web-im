2c9e60ff39d297ef47b0c159b3da6a7046805408
stupidkiller <ooyt@qq.com>
2016-08-02 10:40:46 +0800

    update: update config

--- demo/javascript/dist/webim.config.js
+++ demo/javascript/dist/webim.config.js
@@ -1,21 +1,24 @@
 WebIM.config = {
     /*
-        The global value set for xmpp server
+    * The global value set for xmpp server
     */
-    xmppURL: 'im-api.easemob.com',
+    xmppURL: 'im-api.hyphenate.io',
     /*
-        The global value set for Easemob backend REST API
+    * The global value set for Easemob backend REST API
     */
-    apiURL: (location.protocol === 'https:' ? 'https:' : 'http:') + '//a1.easemob.com',
+    apiURL: (location.protocol === 'https:' ? 'https:' : 'http:') + '//a1.hyphenate.io',
     /*
-        连接时提供appkey
+    *  Provide AppKey for connection
+    *  连接时提供appkey
     */
-    appkey: "easemob-demo#chatdemoui",
+    appkey: "hyphenatedemo#hyphenatedemo",
     /*
+     * Whether to use HTTPS
      * 是否使用https
      */
     https : location.protocol === 'https:',
     /*
+     * Whether to use multiple resources
      * 是否使用多resource
      */
     multiResources: false
