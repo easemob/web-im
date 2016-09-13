802301b68ef5bb423b848a2f9aa23b855e8f9370
wk3368 <wk3368@163.com>
2016-09-13 15:02:39 +0800

    新增 添加成员 的 react-select-box 组件

--- demo/javascript/src/components/leftbar/addGroup.js
+++ demo/javascript/src/components/leftbar/addGroup.js
@@ -10,14 +10,51 @@ var Button = UI.Button;
 var Input = UI.Input;
 var Checkbox = UI.Checkbox;
 
-var AddGroup = React.createClass({
+var SelectBox = React.createFactory(require('react-select-box'));
+var div = React.createElement.bind(null,'div');
+var option = React.createElement.bind(null,'option');
 
+var SelectBoxNode = React.createClass({
+    getInitialState: function () {
+        return {
+            friends: []
+        }
+    },
+    handleMultiChange: function (friends) {
+        this.setState({ friends: friends })
+    },
+    render: function() {
+        var optionNodes = Object.keys(Demo.roster).map(function(name) {
+            return (<option value={name} key={name}>{name}</option>);
+        });
+        return (
+            SelectBox(
+                {
+                    label: Demo.lan.addGroupMember,
+                    onChange: this.handleMultiChange,
+                    value: this.state.friends,
+                    multiple: true,
+                    ref:'select'
+                },
+                optionNodes
+            )
+        );
+    }
+});
+
+var AddGroup = React.createClass({
+    getInitialState: function () {
+        return {
+            colors: []
+        }
+    },
     addSubmit: function () {
         var value = this.refs.input.refs.input.value;
         var info = this.refs.textarea.value;
         var permission_group = this.refs.permission_group.refs.input.checked;
         var permission_member = this.refs.permission_member.refs.input.checked;
-        log(value,info,permission_group,permission_member);
+        var friendsSelected = this.refs.SelectBoxNode.refs.select.value();
+        log(value,info,permission_group,permission_member,friendsSelected);
         if ( !value ) { return; }
 
         Demo.conn.subscribe({
@@ -32,7 +69,6 @@ var AddGroup = React.createClass({
     },
 
     render: function () {
-
         return (
             <div className='webim-friend-options'>
                 <div ref='layer' className='webim-layer'></div>
@@ -42,12 +78,13 @@ var AddGroup = React.createClass({
                         <Input defaultFocus='true' ref='input' placeholder={Demo.lan.groupName} />
                         <br/>
                         <textarea ref='textarea' placeholder={Demo.lan.groupInfo}></textarea>
-                    </div>
-                    <div>
                         <Checkbox text={Demo.lan.groupPermission} ref='permission_group' />
                         <Checkbox text={Demo.lan.groupMemberPermission}  ref='permission_member' />
                     </div>
-                    <Button text={Demo.lan.addGroupMember} onClick={this.addSubmit} className='webim-dialog-button' />
+                    <div>
+                        <SelectBoxNode ref='SelectBoxNode'/>
+                    </div>
+                    <Button text={Demo.lan.add} onClick={this.addSubmit} className='webim-dialog-button' />
                     <span className='font' onClick={this.close}>A</span>
                 </div>
             </div>
