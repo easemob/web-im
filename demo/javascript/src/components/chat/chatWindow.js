c43f1aa0404018a4eb66a781f29ef63e71744f44
wk3368 <wk3368@163.com>
2016-09-20 12:57:28 +0800

    群组的聊天窗口上方的成员下拉列表 兼容widowSDK

--- demo/javascript/src/components/chat/chatWindow.js
+++ demo/javascript/src/components/chat/chatWindow.js
@@ -28,29 +28,43 @@ module.exports = React.createClass({
     },
 
     listMember: function () {
-        var me = this;
-
-
-        if (me.refs.i.className.indexOf('up') < 0) {
-
-            Demo.conn.queryRoomMember({
-                roomId: me.props.roomId,
-                success: function (members) {
-                    if (members && members.length > 0) {
-                        me.refs.i.className = 'webim-down-icon font smallest dib webim-up-icon';
-
-                        me.setState({members: members, memberShowStatus: true});
+        if (this.refs.i.className.indexOf('up') < 0) {
+            var me = this;
+            if (typeof WebIM.config.isWindowSDK === 'boolean' && WebIM.config.isWindowSDK) {
+                //TODO:@李宏儒 群组的聊天窗口上方的成员下拉列表
+                WebIM.doQuery('{"type":"getRoster"}',
+                    function success(str) {
+                        //var str = '{"wenke":1,"wenke2":1}';
+                        var members = eval('(' + str + ')');
+                        if (members && members.length > 0) {
+                            me.refreshMemberList(members);
+                        }
+                    },
+                    function failure(errCode, errMessage) {
+                        alert("listMember" + errCode);
+                    });
+            } else {
+                Demo.conn.queryRoomMember({
+                    roomId: me.props.roomId,
+                    success: function (members) {
+                        if (members && members.length > 0) {
+                            me.refreshMemberList(members);
+                        }
+                    },
+                    error: function () {
                     }
-                },
-                error: function () {
-                }
-            });
+                });
+            }
         } else {
-            me.refs.i.className = 'webim-down-icon font smallest dib';
-            me.setState({members: [], memberShowStatus: false});
+            this.refs.i.className = 'webim-down-icon font smallest dib';
+            this.setState({members: [], memberShowStatus: false});
         }
     },
 
+    refreshMemberList: function (members) {
+        this.refs.i.className = 'webim-down-icon font smallest dib webim-up-icon';
+        this.setState({members: members, memberShowStatus: true});
+    },
     send: function (msg) {
         Demo.conn.send(msg);
         Demo.api.appendMsg(msg, 'txt');
