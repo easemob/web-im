d99cc397b956d22a235e34ea2be7fcd3c981ef96
wk3368 <wk3368@163.com>
2016-09-26 15:56:59 +0800

    益同： 1.图片消息的宽高属性没有带上 2.上次说的多个url替换的问题

--- sdk/src/utils.js
+++ sdk/src/utils.js
@@ -351,21 +351,23 @@
         },
 
         parseLink: function (msg) {
+
             var reg = /(https?\:\/\/|www\.)([a-zA-Z0-9-]+(\.[a-zA-Z0-9]+)+)(\:[0-9]{2,4})?\/?((\.[:_0-9a-zA-Z-]+)|[:_0-9a-zA-Z-]*\/?)*\??[:_#@*&%0-9a-zA-Z-/=]*/gm;
-            var res = msg.match(reg);
-            var src = res && res[0] ? res[0] : '';
-            if (res && res.length) {
-                var prefix = /^https?:\/\//.test(src);
-                msg = msg.replace(reg
-                    , "<a href='"
-                    + (prefix
-                        ? src
-                        : '\/\/' + src)
+
+            msg = msg.replace(reg, function (v) {
+
+                var prefix = /^https?/gm.test(v);
+
+                return "<a href='"
+                    + (prefix ? v : '//' + v)
                     + "' target='_blank'>"
-                    + src
-                    + "</a>");
-            }
+                    + v
+                    + "</a>";
+
+            });
+
             return msg;
+
         },
 
         parseJSON: function (data) {
