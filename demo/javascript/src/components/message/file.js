bd2de61cf2bf2f146187f197352880d4a55d44d1
wk3368 <wk3368@163.com>
2016-09-29 11:25:59 +0800

    http://172.16.0.32:8080/browse/WIN-6 聊天：收到附件消息后点击下载附件消息，没反应

--- demo/javascript/src/components/message/file.js
+++ demo/javascript/src/components/message/file.js
@@ -7,10 +7,18 @@ var FileMsg = React.createClass({
 
     render: function () {
         var icon = this.props.className === 'left' ? 'H' : 'I';
+        var links = [];
+        if (WebIM.config.isWindowSDK) {
+            var dirPath = this.props.value.substring(0, this.props.value.lastIndexOf('/'));
+            links.push(<a target='_blank' href={this.props.value} key='0'>{Demo.lan.openFile}</a>);
+            links.push(<a target='_blank' href={dirPath} key='1' className='dir'>{Demo.lan.openDir}</a>);
+        } else {
+            links.push(<a target='_blank' href={this.props.value}>{Demo.lan.download}</a>);
+        }
 
         return (
             <div className={'rel ' + this.props.className}>
-                <Avatar src={this.props.src} className={this.props.className + ' small'} />
+                <Avatar src={this.props.src} className={this.props.className + ' small'}/>
                 <p className={this.props.className}>{this.props.name} {this.props.time}</p>
                 <div className='webim-msg-value' style={{minWidth: '200px'}}>
                     <span className='webim-msg-icon font'>{icon}</span>
@@ -19,7 +27,7 @@ var FileMsg = React.createClass({
                         <div>
                             <span className='webim-msg-header-icon font small'>S</span>
                             <span className='webim-msg-name'>{this.props.filename}</span>
-                            <a target='_blank' href={this.props.value}>{Demo.lan.download}</a>
+                            {links}
                         </div>
                     </div>
                 </div>
@@ -28,7 +36,7 @@ var FileMsg = React.createClass({
     }
 });
 
-module.exports = function ( options, sentByMe ) {
+module.exports = function (options, sentByMe) {
     var props = {
         src: options.avatar || 'demo/images/default.png',
         time: options.time || new Date().toLocaleString(),
@@ -44,7 +52,7 @@ module.exports = function ( options, sentByMe ) {
     Demo.api.scrollIntoView(node);
 
     return ReactDOM.render(
-		<FileMsg {...props} className={sentByMe ? 'right' : 'left'} />,
-	    node	
-	);
+        <FileMsg {...props} className={sentByMe ? 'right' : 'left'}/>,
+        node
+    );
 };
