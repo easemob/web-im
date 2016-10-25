71e57390273669c38f37f9a876900e3e9716e5a4
wytheme <liu48566203@gmail.com>
2016-10-25 19:35:31 +0800

    web pack prod 发布配置完善

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,23 +1,18 @@
 var webpack = require('webpack');
 path = require('path');
 
-
-console.log(path.resolve('./'));
 module.exports = {
     entry: {
-        // "only" prevents reload on syntax errors
-        './sdk/dist/websdk-1.1.2': ['./sdk/src/connection', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
-        './demo/javascript/dist/demo': ['./demo/javascript/src/entry', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
-        // WebpackDevServer host and port
-        // 'devServerClient': 'webpack-dev-server/client?http://localhost:3000',
+        './sdk/dist/websdk-1.1.2': './sdk/src/connection',
+        './demo/javascript/dist/demo': './demo/javascript/src/entry',
         // './webrtc/dist/webrtc-1.0.0': './webrtc/src/webrtc',
     },
     output: {
-        path: path.resolve('./'),
-        publicPath: '/',
+        path: '../',
+        publicPath: './',
         filename: '[name].js'
     },
-    devtool: '#eval-cheap-module-source-map',
+    // devtool: '#eval-cheap-module-source-map',
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
@@ -27,7 +22,6 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 loader: 'babel',
                 exclude: /node_modules/,
-                include: path.resolve(__dirname, './demo/javascript')
             },
             {
                 test: /\.scss$/,
@@ -40,21 +34,13 @@ module.exports = {
         ]
     },
     plugins: [
-        // used for hot load
-        new webpack.HotModuleReplacementPlugin(),
         // new webpack.NoErrorsPlugin(),
         // minifies your code
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compressor: {
-        //         warnings: false
-        //     }
-        // })
-        new webpack.DefinePlugin({
-            "process.env": {
-                // NODE_ENV: JSON.stringify("production")
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
             }
-        }),
-        // new OpenBrowserPlugin({url: 'http://localhost:3000'})
+        })
     ],
 }
 ;
