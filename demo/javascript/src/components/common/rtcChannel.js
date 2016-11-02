e929811af2de26a594f46d6efa163088b2b51e98
wk3368 <wk3368@163.com>
2016-11-02 19:36:59 +0800

    webrtc UI & UE optimizing

--- demo/javascript/src/components/common/rtcChannel.js
+++ demo/javascript/src/components/common/rtcChannel.js
@@ -5,8 +5,15 @@ var Drag = require('./drag');
 var Channel = React.createClass({
     getInitialState: function () {
         return {
-            local: false,
-            remote: true
+            localFullRemoteCorner: false,
+            toggle_left: 0,
+            toggle_top: 0,
+            toggle_display: 'none',
+            close_right: 0,
+            close_bottom: 0,
+            accept_left: 0,
+            accept_bottom: 0,
+            accept_display: 'block'
         };
     },
 
@@ -32,54 +39,98 @@ var Channel = React.createClass({
         this.props.close();
     },
 
-    acceptCall: function () {
+    accept: function () {
         Demo.call.acceptCall();
     },
 
     toggle: function () {
-        if (this.state.local) {
-            this.setState({local: false, remote: true});
-        } else {
-            this.setState({local: true, remote: false});
-        }
+        this.setState({localFullRemoteCorner: !this.state.localFullRemoteCorner});
     },
 
-    setStream: function () {
-        var me = this;
+    setStream: function (props) {
 
-        me.refs.remoteVideo.srcObject = me.props.remoteStream;
-        me.refs.localVideo.srcObject = me.props.localStream;
+        this.refs.remoteVideo.srcObject = props.remoteStream;
+        this.refs.localVideo.srcObject = props.localStream;
 
-        me.refs.localVideo.oncanplay = function () {
-            me.refs.localVideo.play();
-        };
 
-        me.refs.remoteVideo.oncanplay = function () {
-            me.refs.remoteVideo.play();
-        };
+    },
+
+
+    componentWillReceiveProps: function (nextProps) {
+        console.log('componentWillReceiveProps', nextProps);
+        this.setStream(nextProps);
     },
 
     componentDidUpdate: function () {
-        this.setStream();
+        console.log('did update');
+
+        var me = this;
+        this.refs.localVideo.oncanplay = function () {
+            me.refs.localVideo.play();
+            console.log('localVideo', me.refs.localVideo.getBoundingClientRect());
+
+        };
+
+        this.refs.remoteVideo.oncanplay = function () {
+            me.refs.remoteVideo.play();
+            console.log('remoteVideo', me.refs.remoteVideo.getBoundingClientRect());
+            var rect = me.refs.remoteVideo.getBoundingClientRect();
+            me.setState({
+                close_right: rect.width / 2 - 6,
+                toggle_display: 'block',
+                accept_display: 'none'
+            });
+        };
+
     },
 
     componentDidMount: function () {
+        console.log('did mount');
         new Drag(this.refs.rtc);
+        this.resetButtonPosition();
+    },
 
-        this.setStream();
+    resetButtonPosition: function () {
+        var rect = this.refs.remoteVideo.getBoundingClientRect();
+        this.setState({
+            toggle_left: rect.width / 2 - 6,
+            toggle_top: 6,
+            close_right: 6,
+            close_bottom: 6,
+            accept_left: 6,
+            accept_bottom: 6
+        });
     },
 
     render: function () {
 
-        var localClassName = this.state.local ? 'webim-rtc-video-full' : 'webim-rtc-video-corner';
-        var remoteClassName = this.state.remote ? 'webim-rtc-video-full' : 'webim-rtc-video-corner';
-
+        var localClassName = this.state.localFullRemoteCorner ? 'full' : 'corner';
+        var remoteClassName = this.state.localFullRemoteCorner ? 'corner' : 'full';
         return (
             <div ref='rtc' className='webim-rtc-video'>
-                <video ref='localVideo' className={localClassName} onClick={this.toggle}/>
-                <video ref='remoteVideo' className={remoteClassName} onClick={this.toggle}/>
-                <i className='font small close' onClick={this.close}>Q</i>
-                <i className='font small accept' onClick={this.acceptCall}>z</i>
+                <video ref='localVideo' className={localClassName}/>
+                <video ref='remoteVideo' className={remoteClassName}/>
+                <i ref='close' className='font small' style={{
+                    left: 'auto',
+                    right: this.state.close_right + 'px',
+                    top: 'auto',
+                    bottom: this.state.close_bottom + 'px'
+                }} onClick={this.close}>Q</i>
+                <i ref='accept' className='font small' style={{
+                    display: this.state.accept_display,
+                    left: this.state.accept_left + 'px',
+                    right: 'auto',
+                    top: 'auto',
+                    bottom: this.state.accept_bottom + 'px'
+                }} onClick={this.accept}>z</i>
+                <i ref='toggle' className='font small toggle'
+                   style={{
+                       display: this.state.toggle_display,
+                       left: this.state.toggle_left + 'px',
+                       right: 'auto',
+                       top: this.state.toggle_top + 'px',
+                       bottom: 'auto'
+                   }} onClick={this.toggle}>d</i>
             </div>
         );
     }
