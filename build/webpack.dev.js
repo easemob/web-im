de14124d4951d271eea5d08b4b11736f9830b76a
wk3368 <wk3368@163.com>
2016-12-08 09:30:27 +0800

    1.4.6 publish

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -7,13 +7,13 @@ console.log(path.resolve('./'));
 module.exports = {
     entry: {
         // "only" prevents reload on syntax errors
-        './sdk/dist/websdk-1.4.5': ['./sdk/index', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
+        './sdk/dist/websdk-1.4.6': ['./sdk/index', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
         // './sdk/dist/websdk-1.1.2': './sdk/src/connection',
-        './demo/javascript/dist/demo-1.4.5': ['./demo/javascript/src/entry', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
+        './demo/javascript/dist/demo-1.4.6': ['./demo/javascript/src/entry', 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3000'],
         // './demo/javascript/dist/demo': './demo/javascript/src/entry',
         // WebpackDevServer host and port
         // 'devServerClient': 'webpack-dev-server/client?http://localhost:3000',
-        // './webrtc/dist/webrtc-1.4.5': ['./webrtc/src/entry']
+        // './webrtc/dist/webrtc-1.4.6': ['./webrtc/src/entry']
     },
     externals: {
         WebIM: 'window.WebIM'
