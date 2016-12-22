41b09deb6f050bb8e0d0cecf6b54f70af320300c
clock <zhongzf@easemob.com>
2016-12-22 10:13:03 +0800

    Release 1.4.7

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ path = require('path');
 module.exports = {
     entry: {
         //add [] to fix: Module not found: Error: a dependency to an entry point is not allowed
-        './sdk/dist/websdk-1.4.6': ['./sdk/index'],
-        './demo/javascript/dist/demo-1.4.6': ['./demo/javascript/src/entry'],
-        './webrtc/dist/webrtc-1.4.6': ['./webrtc/src/entry']
+        './sdk/dist/websdk-1.4.7': ['./sdk/index'],
+        './demo/javascript/dist/demo-1.4.7': ['./demo/javascript/src/entry'],
+        './webrtc/dist/webrtc-1.4.7': ['./webrtc/src/entry']
     },
     output: {
         path: './',
