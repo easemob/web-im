5ab9dc110e20deec26ee5e2812793ced69b5e7b1
wk3368 <wk3368@163.com>
2017-05-27 12:18:41 +0800

    webim-interface test     ✓ test1: apiURL-http://a1.easemob.com/easemob-demo/chatdemoui/token     ✓ test2: apiURL-https://a1.easemob.com/easemob-demo/chatdemoui/token     ✓ test3: xmppURL-ws://im-api.easemob.com/ws     ✓ test4: xmppURL-wss://im-api.easemob.com/ws

--- __test__/interface.test.js
+++ __test__/interface.test.js
@@ -18,34 +18,54 @@ beforeEach(() => {
 });
 
 require('../demo/javascript/dist/webim.config.js');
+require('../sdk/dist/strophe-1.2.8.min.js');
+require('../sdk/dist/websdk-1.4.8.js');
+
+log('WebIM.config:', WebIM.config)
+
+let temp1 = WebIM.config.appkey.split('#');
+let orgName = temp1[0];
+let appName = temp1[1];
+
+let temp2 = WebIM.config.apiURL.split('//');
+let apiURL = temp2[1] + '/' + orgName + '/' + appName + '/token';
+
+let username = 'liuwz'
+let password = '1'
 
 
-describe('webim-test', () => {
+let loginJson = {
+    grant_type: 'password',
+    username: username,
+    password: password,
+    timestamp: new Date()
+}
 
-    it('test1: apiURL-rest', done => {
+let xmppURL = WebIM.config.xmppURL + '/ws'
 
-        let username = 'liuwz'
-        let password = '1'
+let Demo = {}
 
-        log('WebIM.config:', WebIM.config)
+Demo.conn = new WebIM.connection({
+    isMultiLoginSessions: WebIM.config.isMultiLoginSessions,
+    https: typeof WebIM.config.https === 'boolean' ? WebIM.config.https : location.protocol === 'https:',
+    url: WebIM.config.xmppURL,
+    heartBeatWait: WebIM.config.heartBeatWait,
+    autoReconnectNumMax: WebIM.config.autoReconnectNumMax,
+    autoReconnectInterval: WebIM.config.autoReconnectInterval,
+    apiUrl: WebIM.config.apiURL,
+    isHttpDNS: WebIM.config.isHttpDNS,
+    isWindowSDK: WebIM.config.isWindowSDK,
+    isAutoLogin: false
+});
 
-        var devInfos = WebIM.config.appkey.split('#');
+log(Demo.conn)
 
+describe('webim-interface test', () => {
 
-        var orgName = devInfos[0];
-        var appName = devInfos[1];
-
-
-        var loginJson = {
-            grant_type: 'password',
-            username: username,
-            password: password,
-            timestamp: new Date()
-        }
-
+    it('test1: apiURL-http://' + apiURL, done => {
 
         $.ajax({
-            url: WebIM.config.apiURL + '/' + orgName + '/' + appName + '/token',
+            url: 'http://' + apiURL,
             type: 'GET',
             data: loginJson,
             error: function (jqXHR, textStatus, errorThrown) {
@@ -66,7 +86,53 @@ describe('webim-test', () => {
 
     })
 
-    it('test1: xmppURL-IM 2.0', done => {
+    it('test2: apiURL-https://' + apiURL, done => {
+
+        $.ajax({
+            url: 'https://' + apiURL,
+            type: 'GET',
+            data: loginJson,
+            error: function (jqXHR, textStatus, errorThrown) {
+                expect(1).toBe(0)
+                if (jqXHR) {
+                    log('test2 error:', jqXHR)
+                }
+                done()
+            },
+            success: function (respData, textStatus, jqXHR) {
+                expect(1).toBe(1)
+                // if (respData) {
+                //     log('test2 success:', respData)
+                // }
+                done()
+            }
+        });
+
+    })
+
+    it('test3: xmppURL-ws://' + xmppURL, done => {
+        var options = {
+            apiUrl: WebIM.config.apiURL,
+            user: username,
+            pwd: password,
+            accessToken: '',
+            appKey: WebIM.config.appkey,
+            success: function (resp) {
+                expect(1).toBe(1)
+                // log('test3 success:', resp)
+                done()
+            },
+            error: function (resp) {
+                expect(1).toBe(0)
+                log('test3 error:', resp)
+                done()
+            }
+        };
+        Demo.conn.open(options);
+
+    })
+
+    it('test4: xmppURL-wss://' + xmppURL, done => {
         expect(1).toBe(1)
         done()
     })
