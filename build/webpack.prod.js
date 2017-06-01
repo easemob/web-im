f47d449082343b02166c9de80439cccc6f48b62e
wytheme <liu48566203@gmail.com>
2017-06-01 12:16:11 +0800

    fix version

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: {
         './sdk/dist/websdk-1.4.11': ['./sdk/index'],
         './demo/javascript/dist/demo-1.4.11': ['./demo/javascript/src/entry'],
-        './webrtc/dist/webrtc-1.4.10': ['./webrtc/src/entry']
+        './webrtc/dist/webrtc-1.4.11': ['./webrtc/src/entry']
     },
     output: {
         path: './',
