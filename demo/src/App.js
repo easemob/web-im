33213ab1833ce63d569239b0845d767ee8cdfe99
wytheme <liu48566203@gmail.com>
2017-07-11 14:59:13 +0800

    add ant.design and eject create-react-app

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+import { Button } from "antd"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Button type="primary">Button</Button>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
