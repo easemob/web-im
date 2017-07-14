c7cd24229d8dcac34d3d59e9d884006418f775ae
wytheme <liu48566203@gmail.com>
2017-07-14 17:50:57 +0800

    add go to definition support for @ with vscode

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,11 +7,14 @@ import registerServiceWorker from "./registerServiceWorker"
 // redux
 import { Provider } from "react-redux"
 import { store } from "@/redux"
+import conf from "@config"
 
 // component
 import Routers from "./Routers"
 
 import "./theme.less"
+
+console.log(conf)
 
 ReactDOM.render(
 	<Provider store={store}>
