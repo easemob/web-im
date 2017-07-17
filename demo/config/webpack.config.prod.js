f26ca40597b1f12bf24f3c4ec07684a3847eabe3
wytheme <liu48566203@gmail.com>
2017-07-17 14:39:32 +0800

    remove eslint check when build

--- demo/config/webpack.config.prod.js
+++ demo/config/webpack.config.prod.js
@@ -112,19 +112,19 @@ module.exports = {
 
 			// First, run the linter.
 			// It's important to do this before Babel processes the JS.
-			{
-				test: /\.(js|jsx)$/,
-				enforce: "pre",
-				use: [
-					{
-						options: {
-							formatter: eslintFormatter
-						},
-						loader: require.resolve("eslint-loader")
-					}
-				],
-				include: paths.appSrc
-			},
+			// {
+			// 	test: /\.(js|jsx)$/,
+			// 	enforce: "pre",
+			// 	use: [
+			// 		{
+			// 			options: {
+			// 				formatter: eslintFormatter
+			// 			},
+			// 			loader: require.resolve("eslint-loader")
+			// 		}
+			// 	],
+			// 	include: paths.appSrc
+			// },
 			// ** ADDING/UPDATING LOADERS **
 			// The "file" loader handles all assets unless explicitly excluded.
 			// The `exclude` list *must* be updated with every change to loader extensions.
