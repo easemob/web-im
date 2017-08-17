28763d0a6889a3194e666d094d01b1f739a531ab
leopku <leopku@qq.com>
2017-08-17 12:53:04 +0800

    添加 eslint 和 editorconfig 配置
    1. eslint 可通过 IDE 插件或命令行可实现规范 JS 书写
    2. editorconfig 通过 IDE 插件规范缩进、换行等

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Demo": true,
+        "WebIM": true
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
