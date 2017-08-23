81406a52e71f25276774ffa2a6cfe588afa7a0a6
wk3368 <wk3368@163.com>
2017-08-23 14:57:06 +0800

    解决android手机浏览器兼容性问题

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,31 +4,33 @@ import ReactDOM from "react-dom"
 import "./App.css"
 import "./themes/theme.less"
 import registerServiceWorker from "./registerServiceWorker"
-import { history } from "@/utils"
+import {history} from "@/utils"
 
 import {
-	// BrowserRouter as Router,
-	// HashRouter as Router
-	Router
+    // BrowserRouter as Router,
+    // HashRouter as Router
+    Router
 } from "react-router-dom"
 // redux
-import { Provider } from "react-redux"
-import { store } from "@/redux"
+import {Provider} from "react-redux"
+import {store} from "@/redux"
 import App from "./App"
+// fix android browsers compatibilities
+import 'babel-polyfill'
 
 var FastClick = require("fastclick")
 FastClick.attach(document.body)
 
 const rootEl = document.getElementById("root")
 const render = Component =>
-	ReactDOM.render(
-		<Provider store={store}>
-			<Router history={history}>
-				<Component />
-			</Router>
-		</Provider>,
-		rootEl
-	)
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router history={history}>
+                <Component />
+            </Router>
+        </Provider>,
+        rootEl
+    )
 
 render(App)
 if (module.hot) module.hot.accept("./App", () => render(App))
