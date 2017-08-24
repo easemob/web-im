a353e9d32e23e132ad47bac3b35906a917c1ed70
leopku <leopku@qq.com>
2017-08-24 23:17:45 +0800

    optimize group member list

--- demo/src/components/group/groupMembers.js
+++ demo/src/components/group/groupMembers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {Card, Menu} from 'antd';
+import {Card, Menu, Table} from 'antd';
 import dottie from 'dottie';
 import './style/index.less';
 
@@ -8,11 +8,18 @@ class GroupMembers extends React.Component {
     render () {
         const {entities, roomId} = this.props;
         const members = dottie.get(entities, `groupMember.${roomId}.names`, []);
+        const columns = [{
+            title: 'Name',
+            key: 'name',
+            dataIndex: 'name'
+        }]
+        const data = members.map(val => { return {name: val} })
         return (
             <Card title="Members" bordered={false} noHovering={true}>
-                <Menu className="group-member-list">
+                {/* <Menu className="group-member-list">
                     {members.map((val, idx) => <Menu.Item key={idx} className="group-member-item"><span>{val}</span></Menu.Item>)}
-                </Menu>
+                </Menu> */}
+                <Table columns={columns} dataSource={data} showHeader={false} pagination={false} scroll={{y:300}}></Table>
             </Card>
         );
     }
