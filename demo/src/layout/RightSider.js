1cf6f6a956bf072cb7b3969575291bf8e98f12ea
leopku <leopku@qq.com>
2017-08-24 10:30:50 +0800

    dissolve group

--- demo/src/layout/RightSider.js
+++ demo/src/layout/RightSider.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
+import {connect} from 'react-redux';
 import GroupInfo from '@/components/group/groupInfo';
 import GroupMembers from '@/components/group/groupMembers';
 import {config} from '@/config';
 
 class RightSider extends React.Component {
-    static defaultProps = {
-        prefixCls: 'x-layout-right-sider',
-        style: {}
-    };
+    // static defaultProps = {
+    //     prefixCls: 'x-layout-right-sider',
+    //     style: {}
+    // };
 
     constructor(props) {
         super(props);
@@ -23,13 +24,17 @@ class RightSider extends React.Component {
 
     render() {
         const joinPermission = {a: 'aa'};
+        const { entities, roomId } = this.props;
         return (
             <div>
-                <GroupInfo title="Group Info" name="bernard" owner="adsf" description="asdf" joinPermission="joinPermission"></GroupInfo>
-                <GroupMembers title="Members" name="bernard" owner="adsf" description="asdf" joinPermission="joinPermission"></GroupMembers>
+                <GroupInfo title="Group Info" room={this.props.room} owner="adsf" description="asdf" joinPermission="joinPermission"></GroupInfo>
+                <GroupMembers title="Members" roomId={roomId} name={this.props.selectItem} owner="adsf" description="asdf" joinPermission="joinPermission"></GroupMembers>
             </div>
         );
     }
 };
 
-export default RightSider;
+// export default RightSider;
+export default connect(
+    ({entities}) => ({entities})
+)(RightSider)
