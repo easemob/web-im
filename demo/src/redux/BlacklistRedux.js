0e3587ba43b33ffd7045c87ba731c7135b9f3b8d
wytheme <liu48566203@gmail.com>
2017-08-28 16:54:55 +0800

    fix bug add blacklist only will reset other blacklist users

--- demo/src/redux/BlacklistRedux.js
+++ demo/src/redux/BlacklistRedux.js
@@ -8,58 +8,58 @@ import CommonActions from "./CommonRedux"
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
-	updateBlacklist: ["list"],
-	// ----------------async------------------
-	// 更新黑名单列表
-	getBlacklist: () => {
-		return (dispatch, getState) => {
-			WebIM.conn.getBlacklist()
-		}
-	},
-	// 添加到黑名单
-	doAddBlacklist: id => {
-		return (dispatch, getState) => {
-			dispatch(CommonActions.fetching())
+    updateBlacklist: ["list"],
+    // ----------------async------------------
+    // 更新黑名单列表
+    getBlacklist: () => {
+        return (dispatch, getState) => {
+            WebIM.conn.getBlacklist()
+        }
+    },
+    // 添加到黑名单
+    doAddBlacklist: id => {
+        return (dispatch, getState) => {
+            dispatch(CommonActions.fetching())
 
-			let blacklist = getState().entities.blacklist.byName.asMutable()
-			let roster = getState().entities.roster.byName
-			if (blacklist[id]) return
-			blacklist[id] = roster[id].asMutable()
-			WebIM.conn.addToBlackList({
-				list: blacklist,
-				type: "jid",
-				success: function() {
-					// TODO: 之前添加当前黑名单用户还是重新拉取黑名单
-					dispatch(CommonActions.fetched())
-				},
-				error: function() {
-					dispatch(CommonActions.fetched())
-				}
-			})
-		}
-	},
-	// 从黑名单删除
-	doRemoveBlacklist: id => {
-		return (dispatch, getState) => {
-			dispatch(CommonActions.fetching())
+            let blacklist = getState().entities.blacklist.byName.asMutable()
+            let roster = getState().entities.roster.byName
+            if (blacklist[id]) return
+            blacklist[id] = roster[id]
+            WebIM.conn.addToBlackList({
+                list: blacklist,
+                type: "jid",
+                success: function() {
+                    // TODO: 之前添加当前黑名单用户还是重新拉取黑名单
+                    dispatch(CommonActions.fetched())
+                },
+                error: function() {
+                    dispatch(CommonActions.fetched())
+                }
+            })
+        }
+    },
+    // 从黑名单删除
+    doRemoveBlacklist: id => {
+        return (dispatch, getState) => {
+            dispatch(CommonActions.fetching())
 
-			let blacklist = getState().entities.blacklist.byName.asMutable()
-			delete blacklist[id]
-			WebIM.conn.removeFromBlackList({
-				list: blacklist,
-				type: "jid",
-				success: function() {
-					// TODO: 之前添加当前黑名单用户还是重新拉取黑名单
-					// 其实此处可以支持同步完成，只不过sdk中写的代码也自持listen
-					// 就直接使用listen，但是都没有做增量更新blacklist的操作
-					dispatch(CommonActions.fetched())
-				},
-				error: function() {
-					dispatch(CommonActions.fetched())
-				}
-			})
-		}
-	}
+            let blacklist = getState().entities.blacklist.byName.asMutable()
+            delete blacklist[id]
+            WebIM.conn.removeFromBlackList({
+                list: blacklist,
+                type: "jid",
+                success: function() {
+                    // TODO: 之前添加当前黑名单用户还是重新拉取黑名单
+                    // 其实此处可以支持同步完成，只不过sdk中写的代码也自持listen
+                    // 就直接使用listen，但是都没有做增量更新blacklist的操作
+                    dispatch(CommonActions.fetched())
+                },
+                error: function() {
+                    dispatch(CommonActions.fetched())
+                }
+            })
+        }
+    }
 })
 
 export const BlacklistTypes = Types
@@ -68,23 +68,24 @@ export default Creators
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
-	byName: {},
-	names: []
+    byName: {},
+    names: []
 })
 
 /* ------------- Reducers ------------- */
 
 export const updateBlacklist = (state, { list }) => {
-	return state.merge({
-		byName: list,
-		names: Object.keys(list).sort()
-	})
+    console.log("updateBlacklist", list)
+    return state.merge({
+        byName: Object(list),
+        names: Object.keys(list).sort()
+    })
 }
 
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
-	[Types.UPDATE_BLACKLIST]: updateBlacklist
+    [Types.UPDATE_BLACKLIST]: updateBlacklist
 })
 
 /* ------------- Selectors ------------- */
