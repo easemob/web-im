7f4816b3f78bf071d944671d1dc893577d5a5b7c
wk3368 <wk3368@163.com>
2017-09-01 18:47:23 +0800

    change block list height from 100px to 200px

--- demo/src/components/friend/FriendsRequestModal.js
+++ demo/src/components/friend/FriendsRequestModal.js
@@ -9,58 +9,59 @@ import { I18n } from "react-redux-i18n"
 import _ from "lodash"
 
 class FriendsRequestModal extends React.Component {
-	state = {}
-	render() {
-		const { declineSubscribe, acceptSubscribe } = this.props
-		const requests = []
+    state = {}
 
-		_.forEach(this.props.data, ({ from, status }) => {
-			requests.push(
-				<Row key={from}>
-					<Col span={14}>
-						{from + ": " + status}
-					</Col>
-					<Col span={10}>
-						<Button
-							style={{
-								height: 32,
-								marginLeft: 10
-							}}
-							className="fr"
-							type="primary"
-							onClick={() => acceptSubscribe(from)}
-						>
-							{I18n.t("agree")}
-						</Button>
-						<Button
-							style={{
-								height: 32
-							}}
-							className="fr"
-							type="danger"
-							onClick={() => declineSubscribe(from)}
-						>
-							{I18n.t("reject")}
-						</Button>
-					</Col>
-				</Row>
-			)
-		})
+    render() {
+        const { declineSubscribe, acceptSubscribe } = this.props
+        const requests = []
 
-		return (
-			<div>
-				{requests}
-			</div>
-		)
-	}
+        _.forEach(this.props.data, ({ from, status }) => {
+            requests.push(
+                <Row key={from}>
+                    <Col span={14}>
+                        {from + ": " + status}
+                    </Col>
+                    <Col span={10}>
+                        <Button
+                            style={{
+                                height: 32,
+                                marginLeft: 10
+                            }}
+                            className="fr"
+                            type="primary"
+                            onClick={() => acceptSubscribe(from)}
+                        >
+                            {I18n.t("agree")}
+                        </Button>
+                        <Button
+                            style={{
+                                height: 32
+                            }}
+                            className="fr"
+                            type="danger"
+                            onClick={() => declineSubscribe(from)}
+                        >
+                            {I18n.t("reject")}
+                        </Button>
+                    </Col>
+                </Row>
+            )
+        })
+
+        return (
+            <div>
+                {requests}
+            </div>
+        )
+    }
 }
 
 export default connect(
-	({ entities }) => ({
-		data: entities.subscribe.byFrom
-	}),
-	dispatch => ({
-		acceptSubscribe: id => dispatch(SubscribeActions.acceptSubscribe(id)),
-		declineSubscribe: id => dispatch(SubscribeActions.declineSubscribe(id))
-	})
+    ({ entities }) => ({
+        data: entities.subscribe.byFrom
+    }),
+    dispatch => ({
+        acceptSubscribe: id => dispatch(SubscribeActions.acceptSubscribe(id)),
+        declineSubscribe: id => dispatch(SubscribeActions.declineSubscribe(id))
+    })
 )(FriendsRequestModal)
