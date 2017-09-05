e606c4d27b907e11d66bbcf42ba3b65aed37e70f
wk3368 <wk3368@163.com>
2017-09-06 01:51:10 +0800

    Demo.call -> WebIM.call

--- demo/src/containers/chat/Chat.js
+++ demo/src/containers/chat/Chat.js
@@ -21,7 +21,7 @@ import WebIM from "@/config/WebIM"
 import { message } from "antd"
 import { history } from "@/utils"
 import getTabMessages from "@/selectors/ChatSelector"
-
+import RTCChannel from "@/components/common/rtcChannel"
 
 const { TextArea } = Input
 const FormItem = Form.Item
@@ -284,12 +284,126 @@ class Chat extends React.Component {
 
     componentDidMount() {
         this.scollBottom()
-        console.log("componentDidMount", WebIM.config.isWebRTC, WebIM.WebRTC)
         if (WebIM.config.isWebRTC && WebIM.WebRTC) {
-            console.log("dsd")
-            // this.initWebRTC();
-            // this.channel = new RTCChannel(this.refs.rtcWrapper);
+            this.initWebRTC()
+            this.channel = new RTCChannel(this.refs.rtcWrapper)
         }
+    }
+
+    rtcTimeoutID = null
+
+    initWebRTC() {
+
+        if (WebIM.call) {
+            return
+        }
+
+        var me = this
+
+        WebIM.call = new WebIM.WebRTC.Call({
+            connection: WebIM.conn,
+
+            mediaStreamConstaints: {
+                audio: true,
+                video: true
+            },
+
+            listener: {
+                onOtherUserOpenVoice: function (from, opened) {
+                    console.log("from open:", opened, " voice .", from)
+                },
+                onOtherUserOpenVideo: function (from, opened) {
+                    console.log("from open:", opened, " voideo .", from)
+                },
+                onAcceptCall: function (from, options, enableVoice, enableVideo) {
+                    console.log("onAcceptCall", from, options, enableVoice, enableVideo)
+                },
+                onGotRemoteStream: function (stream, streamType) {
+                    console.log("onGotRemoteStream")
+                    me.channel.setRemote(stream, streamType)
+                },
+                onGotLocalStream: function (stream, streamType) {
+                    console.log("onGotLocalStream ", "Stream Type: ", streamType)
+                    me.channel.setLocal(stream, streamType)
+                },
+                onRinging: function (caller, streamType) {
+                    console.log("onRinging", caller)
+                    me.channel.ringing(caller, streamType)
+                },
+                onTermCall: function (reason) {
+                    //"ok"      -> 'HANGUP'     "success" -> 'HANGUP'   "timeout"          -> 'NORESPONSE'
+                    //"decline" -> 'REJECT'     "busy"    -> 'BUSY'     "failed-transport" -> 'FAIL'
+                    // TODO reason undefine if reason is busy
+                    console.log("onTermCall")
+                    if (reason && (reason == "busy" || reason == "BUSY")) {
+                        message.error("Target is busy. Try it later.")
+                    }
+                    if (reason && (reason == "timeout" || reason == "NORESPONSE")) {
+                        message.error("Target no response. Try it later.")
+                    }
+                    if (reason && (reason == "decline" || reason == "REJECT")) {
+                        message.error("Target reject.")
+                    }
+                    if (reason && (reason == "failed-transport" || reason == "FAIL")) {
+                        message.error("Call failed. Try it later.")
+                    }
+                    if (reason && (reason == "ok" || reason == "success" || reason == "HANGUP")) {
+                        message.success("Target hangup. ")
+                    }
+
+
+                    WebIM.call.caller = ""
+                    WebIM.call.callee = ""
+                    me.channel.close()
+                },
+                onIceConnectionStateChange: function (iceState) {
+                    console.log("onIceConnectionStateChange")
+                    // checking
+                    // connected completed
+                    // disconnected failed
+                    // closed
+                    // console.log('onIceConnectionStateChange', iceState);
+                    if (iceState == "disconnected") {
+                        if (!me.rtcTimeoutID) {
+                            //console.warn("Warn. disconnect. notify offline");
+
+                            me.rtcTimeoutID = setTimeout(function () {
+                                if (!(WebIM.call.pattern && WebIM.call.pattern.hangup)) {
+                                    message.success("Target is offline")
+                                    var closeButton = document.getElementById("webrtc_close")
+                                    closeButton && closeButton.click()
+                                }
+                            }, 10000)
+                        }
+                    } else if (iceState == "connected") {
+                        if (me.rtcTimeoutID) {
+                            clearTimeout(me.rtcTimeoutID)
+                            me.rtcTimeoutID = null
+                        }
+                    }
+                },
+                onError: function (e) {
+                    if (e && e.message) {
+                        var close = false
+                        switch (e.message) {
+                        case "CALLLING_EACH_OTHER_AT_THE_SAME_TIME":
+                            e.message = "Target is calling. Please try again later."
+                            close = true
+                            break
+                        case "TARGET_OFFLINE":
+                            e.message = "Target is offline."
+                            break
+                        }
+                        if (close) {
+                            var closeButton = document.getElementById("webrtc_close")
+                            closeButton && closeButton.click()
+                        }
+                    }
+                    message.error(e && e.message ? e.message : "An error occured when calling webrtc")
+                }
+            }
+        })
+
     }
 
     componentWillUnmount() {
@@ -299,15 +413,15 @@ class Chat extends React.Component {
     callVideo = () => {
         const { selectItem } = _.get(this.props, [ "match", "params" ], {})
         console.log("sendWrapper::callVideo", WebIM.conn.context.userId, selectItem)
-        // Demo.call.caller = Demo.user
-        // Demo.call.makeVideoCall(Demo.selected)
+        WebIM.call.caller = WebIM.conn.context.userId
+        WebIM.call.makeVideoCall(selectItem)
     }
 
     callVoice = () => {
         const { selectItem } = _.get(this.props, [ "match", "params" ], {})
         console.log("sendWrapper::callVoice", WebIM.conn.context.userId, selectItem)
-        // Demo.call.caller = Demo.user
-        // Demo.call.makeVoiceCall(Demo.selected)
+        WebIM.call.caller = WebIM.conn.context.userId
+        WebIM.call.makeVoiceCall(selectItem)
     }
 
     render() {
