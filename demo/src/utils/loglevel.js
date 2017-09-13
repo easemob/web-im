526ba672cd3f3ddf8ed48c8e94f03ccb2f10cb4c
leopku <leopku@qq.com>
2017-09-13 13:57:35 +0800

    fixed loglevel template

--- demo/src/utils/loglevel.js
+++ demo/src/utils/loglevel.js
@@ -3,6 +3,6 @@ import prefix from "loglevel-plugin-prefix"
 import config from "../config/WebIMConfig"
 
 loglevel.setLevel(config.loglevel)
-prefix.apply(loglevel, { template: "[%t] %l (%n) static text:" })
+prefix.apply(loglevel, { template: "[%t] %l (%n) logger: " })
 
 export default loglevel
